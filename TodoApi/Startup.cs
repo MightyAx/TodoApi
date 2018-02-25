e417cadbb1982a8db3546ea72bac825089dbe671
Alexie Staffer <MightyAx@users.noreply.github.com>
2018-02-25 16:33:27 +0000

    Entity Framework Implementation

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApi.Models;
 namespace TodoApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("ToDoDatabase")));
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app)
